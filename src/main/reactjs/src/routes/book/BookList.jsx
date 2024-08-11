387a6da46193e0f8f5590ad90fa4d76b2bef2cf3
ynnotun <ynnotun@gmail.com>
2024-08-11 12:03:31 +0900

    파일명 수정

---
+++ src/main/reactjs/src/routes/book/BookList.jsx
@@ -0,0 +1,16 @@
+
+
+export default function List() {
+    const error = useRouteError(); // 발생한 오류를 제공
+    console.error(error);
+
+    return (
+        <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+        </div>
+    );
+}
