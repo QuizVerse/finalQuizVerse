4d045db2c9c6370fa4ee3b1b2a21bb53fd7aa68b
ynnotun <ynnotun@gmail.com>
2024-08-21 12:18:30 +0900

    평가 모달 완료

--- src/main/reactjs/src/components/Review.jsx
+++ src/main/reactjs/src/components/Review.jsx
@@ -1,5 +1,33 @@
+// v0 by Vercel.
+// https://v0.dev/t/dyAI4Y9Tu3s
+
+import {Rating, TextField} from "@mui/material";
+import {useState} from "react";
+
 export default function Review() {
+
+    const [value, setValue] = useState(2);
     return (
-        <></>
+        <>
+            <div className="p-6 space-y-4">
+                <Rating
+                    name="simple-controlled"
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                />
+                <div>
+                    <TextField
+                        id="standard-multiline-static"
+                        label="평가 내용"
+                        multiline
+                        rows={4}
+                        defaultValue="Default Value"
+                        variant="standard"
+                    />
+                </div>
+            </div>
+        </>
     );
 }
---
+++ src/main/reactjs/src/routes/devtest/ReviewModal.jsx
@@ -0,0 +1,50 @@
+import CustomConfirm from "../../components/CustomConfirm";
+import SectionSort from "../../components/SectionSort";
+import {Button} from "@mui/material";
+import {useState} from "react";
+import axios from "axios";
+import Review from "../../components/Review";
+
+export default function ReviewModal() {
+    // confirm state
+    const [confirmVisible, setConfirmVisible] = useState(false);
+
+    /**
+     * @description : Confirm창 열릴 때
+     * */
+    const openConfirm = () => {
+        setConfirmVisible(true);
+    };
+
+    /**
+     * @description : 취소 버튼 클릭시 실행되는 로직
+     * */
+    const clickBtn1 = () => {
+        setConfirmVisible(false);
+    };
+
+    /**
+     * @description : 확인 버튼 클릭시 실행되는 로직
+     * */
+    const clickBtn2 = () => {
+        setConfirmVisible(false);
+    };
+
+
+    return (
+        <>
+            <Button
+                onClick={openConfirm}
+                className="whitespace-nowrap">
+                평가 모달 열기
+            </Button>
+            <CustomConfirm
+                id={13}
+                content={<Review/>}
+                openConfirm={confirmVisible}
+                clickBtn1={clickBtn1}
+                clickBtn2={clickBtn2}
+            ></CustomConfirm>
+        </>
+    );
+}
