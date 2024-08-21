327792970fec47cb57a5226417d6c3eafcf0aae8
ynnotun <ynnotun@gmail.com>
2024-08-21 11:05:22 +0900

    myclass pagination 적용

--- src/main/reactjs/src/routes/mypage/Myclass.jsx
+++ src/main/reactjs/src/routes/mypage/Myclass.jsx
@@ -2,10 +2,11 @@
 // https://v0.dev/t/PyQHzVi1rPb
 
 import CustomConfirm from "../../components/CustomConfirm";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {
   Button,
-  MenuItem,
+  MenuItem, Pagination,
+  Stack,
   Table,
   TableBody,
   TableCell,
@@ -37,6 +38,9 @@ const conditions = [
     label: '제목순',
   },
 ];
+
+const ITEMS_PER_PAGE = 10;
+const SPACING = 2;
 
 export default function Myclass() {
 
@@ -124,26 +128,39 @@ export default function Myclass() {
       setClassDescription('');
       setAlertTitle("클래스 생성이 완료되었습니다.");
       openAlert();
+      getDataList(); // 생성하고나면 리스트 재출력
     })
     setConfirmVisible(false);
   };
+
+  const [page, setPage] = useState(1);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+    window.scrollTo(0, 0);
+  };
+
+  const itemOffset = (page - 1) * ITEMS_PER_PAGE;
+  const currentItems = classList.slice(itemOffset, itemOffset + ITEMS_PER_PAGE);
+  const pageCount = Math.ceil(classList.length / ITEMS_PER_PAGE);
+
 
   return (
       <main className="flex-1 p-6">
 
         <h1 className="mb-6 text-2xl font-bold">나의 클래스</h1>
         <div className="flex items-center mb-4 space-x-4">
-            <TextField
-                id="outlined-select-currency"
-                select
-                defaultValue="popular"
-            >
-              {conditions.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-              ))}
-            </TextField>
+          <TextField
+              id="outlined-select-currency"
+              select
+              defaultValue="popular"
+          >
+            {conditions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+            ))}
+          </TextField>
           <Button
               onClick={openConfirm}
               variant="contained"
@@ -177,272 +194,49 @@ export default function Myclass() {
             className="rounded-lg border bg-card text-card-foreground shadow-sm"
             data-v0-t="card"
         >
-          <div className="relative w-full overflow-auto">
-            <table className="w-full caption-bottom text-sm">
-              <thead className="[&amp;_tr]:border-b">
-              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  이름
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  구성원 수
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  가입일시
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  생성일시
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  상태
-                </th>
-              </tr>
-              </thead>
-              <tbody className="[&amp;_tr:last-child]:border-0">
-              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
-                  정치가 스터디
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  10
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2024.07.31
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2022.11.06
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  <div
-                      className="inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80"
-                      data-v0-t="badge"
-                  >
-                    멤버
-                  </div>
-                </td>
-              </tr>
-              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
-                  자바 함께해요
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  3
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2024.07.26
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2020.07.06
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  <div
-                      className="inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80"
-                      data-v0-t="badge"
-                  >
-                    멤버
-                  </div>
-                </td>
-              </tr>
-              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
-                  영어 스터디
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  5
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2024.07.18
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2021.08.09
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  <div
-                      className="inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80"
-                      data-v0-t="badge"
-                  >
-                    멤버
-                  </div>
-                </td>
-              </tr>
-              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
-                  언어교환
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  9
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2024.07.16
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2017.07.20
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  <div
-                      className="inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80"
-                      data-v0-t="badge"
-                  >
-                    멤버
-                  </div>
-                </td>
-              </tr>
-              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-medium">
-                  백공모드
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  1
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2024.07.06
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  2013.01.16
-                </td>
-                <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-                  <div
-                      className="inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80"
-                      data-v0-t="badge"
-                  >
-                    클래스장
-                  </div>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
+          <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>이름</TableCell>
+                  <TableCell>구성원수</TableCell>
+                  <TableCell>가입일시</TableCell>
+                  <TableCell>생성일시</TableCell>
+                  <TableCell>상태</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {currentItems &&
+                    currentItems.map((row) => (
+                        <TableRow
+                            key={row.class_id}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                        >
+                          <TableCell component="th" scope="row">
+                            {row.class_name}
+                          </TableCell>
+                          <TableCell>{row.class_description}</TableCell>
+                          <TableCell>{row.class_createdate}</TableCell>
+                          <TableCell>{row.carbs}</TableCell>
+                          <TableCell>{row.protein}</TableCell>
+                        </TableRow>
+                    ))
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>이름</TableCell>
-                <TableCell>구성원수</TableCell>
-                <TableCell>가입일시</TableCell>
-                <TableCell>생성일시</TableCell>
-                <TableCell>상태</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {classList.map((row) => (
-                  <TableRow
-                      key={row.class_id}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {
-                        row.class_name}
-                    </TableCell>
-                    <TableCell>{row.class_description}</TableCell>
-                    <TableCell>{row.class_createdate}</TableCell>
-                    <TableCell>{row.carbs}</TableCell>
-                    <TableCell>{row.protein}</TableCell>
-                  </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <div className="flex justify-center mt-4">
-          <nav className="flex items-center space-x-2">
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="w-4 h-4"
-              >
-                <path d="m15 18-6-6 6-6"></path>
-              </svg>
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="w-4 h-4"
-              >
-                <path d="m15 18-6-6 6-6"></path>
-              </svg>
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 w-10">
-              1
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              2
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              3
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              4
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              5
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              6
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              7
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="w-4 h-4"
-              >
-                <path d="m9 18 6-6-6-6"></path>
-              </svg>
-            </button>
-            <button
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="w-4 h-4"
-              >
-                <path d="m9 18 6-6-6-6"></path>
-              </svg>
-            </button>
-          </nav>
+        <div className={"flex justify-center mt-4"}>
+          <Stack spacing={SPACING}>
+            <Pagination
+                count={pageCount}
+                page={page}
+                onChange={handleChange}
+                showFirstButton
+                showLastButton
+            />
+          </Stack>
         </div>
       </main>
-  );
+);
 }
