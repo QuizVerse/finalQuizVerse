8a993c0a31bb91ccbfa5f4def1ddfd392ab389cb
sanghyuck <djfh611@naver.com>
2024-08-26 00:49:09 +0900

    list

--- src/main/reactjs/src/routes/book/BookList.jsx
+++ src/main/reactjs/src/routes/book/BookList.jsx
@@ -7,13 +7,119 @@ import BookCard from "../../components/BookCard";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import {Link} from "react-router-dom";
 import CustomInput from "../../components/CustomInput";
+import Questionmark from "../../image/Questionmark.jpg";
 
+
+//1 초등 2 중고등 3 취업/자격증 4 컴퓨터 5 기타
 export default function BookList() {
   return (
       <main className="p-16">
         <div className="flex items-center justify-center mb-8">
           <div className="w-full max-w-4xl p-16 text-center bg-gray-200 rounded">문제집 추천배너</div>
         </div>
+        <section className="mb-8">
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="text-xl font-bold">초등 문제집 Top 5</h2>
+            <Link className="text-gray-600 flex gap-2 items-center">
+              전체보기
+              <ArrowForwardIosIcon fontSize={'8px'}/>
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="정상혁의 성공률 0%"
+            createDate="2024-08-25"
+            title="타이틀임"
+            category="카테고리"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+          </div>
+        </section>
+        <section className="mb-8">
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="text-xl font-bold">중고등 문제집 Top 5</h2>
+            <Link className="text-gray-600 flex gap-2 items-center">
+              전체보기
+              <ArrowForwardIosIcon fontSize={'8px'}/>
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+          </div>
+        </section>
         <section className="mb-8">
           <div className="flex items-center justify-between mb-4">
             <h2 className="text-xl font-bold">취업/자격증 문제집 Top 5</h2>
@@ -63,14 +169,61 @@ export default function BookList() {
         </section>
         <section className="mb-8">
           <div className="flex items-center justify-between mb-4">
-            <h2 className="text-xl font-bold">초등 문제집 Top 5</h2>
+            <h2 className="text-xl font-bold">컴퓨터 문제집 Top 5</h2>
             <Link className="text-gray-600 flex gap-2 items-center">
               전체보기
               <ArrowForwardIosIcon fontSize={'8px'}/>
             </Link>
           </div>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-          <BookCard cardType="A"
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+            <BookCard cardType="A"
+            nickname="합격률 80%"
+            createDate="2024-08-23"
+            title="2024 정보처리기사 실기"
+            category="문제집/자격증"
+            viewCount="10"
+            questionCount="20"
+            sectionCoune="4"
+            status="여긴status"/>
+          </div>
+        </section>
+        <section className="mb-8">
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="text-xl font-bold">기타 문제집 Top 5</h2>
+            <Link className="text-gray-600 flex gap-2 items-center">
+              전체보기
+              <ArrowForwardIosIcon fontSize={'8px'}/>
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+           <BookCard cardType="A"
             nickname="합격률 80%"
             createDate="2024-08-23"
             title="2024 정보처리기사 실기"
