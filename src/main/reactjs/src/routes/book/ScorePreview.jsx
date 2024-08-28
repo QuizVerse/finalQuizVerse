23122137bf2df90354f07bf4c9f4212e7559dd5e
alswl11 <parkminji08@naver.com>
2024-08-28 14:51:45 +0900

    파일 정리

--- src/main/reactjs/src/routes/book/ScorePreview.jsx
+++ src/main/reactjs/src/routes/book/ScorePreview.jsx
@@ -10,7 +10,7 @@ import LoadingModal from "../../components/modal/LoadingModal";
 
 // 페이지 컴포넌트 정의
 const PdfPage = ({ answers }) => (
-  <div style={{ padding: "10mm", boxSizing: "border-box" }}>
+  <div >
     <div className="overflow-x-auto pt-5 py-8">
       <table className="min-w-full">
         <thead>
@@ -97,7 +97,6 @@ const PdfPage = ({ answers }) => (
                 );
 
                 return (
-                  // fragment 단위로 묶어서 페이지 넘겨도 나올 수 있도록
                   <React.Fragment key={pageIndex}>
                     <tr>
                       {pageAnswers.map((answer, index) => (
@@ -130,26 +129,21 @@ const PdfPage = ({ answers }) => (
           </tbody>
         </table>
       </div>
-    </div>
+    </div>햣
   </div>
 );
 
 export default function ScorePreview() {
   const navigate = useNavigate();
   const printpdf = useRef(null);
+  const [loadingVisible, setLoadingVisible] = useState(false);
 
-  // 문제 갯수
   const answers = Array.from({ length: 222 }, (_, index) => ({
     number: `${index + 1}번`,
     correct: true,
   }));
 
-  // 로딩 모달 상태 관리
-  const [loadingVisible, setLoadingVisible] = useState(false);
-
-  // pdf 추출 사이즈를 a4로
   const downloadpdf = async () => {
-    // PDF 생성중 로딩 모달을 표시
     setLoadingVisible(true);
 
     const pdf = new jsPDF({
@@ -158,12 +152,10 @@ export default function ScorePreview() {
       format: "a4",
     });
 
-    // 여백
     const margin = 10;
     const pdfWidth = pdf.internal.pageSize.getWidth();
     const pdfHeight = pdf.internal.pageSize.getHeight();
 
-    // 문제를 80개씩 chunks라는 배열로 나누기
     const chunks = Array.from(
       { length: Math.ceil(answers.length / 80) },
       (_, i) => answers.slice(i * 80, i * 80 + 80)
@@ -171,32 +163,23 @@ export default function ScorePreview() {
 
     for (let i = 0; i < chunks.length; i++) {
       const chunk = chunks[i];
-
-      // 아래 변환된 DOM을 받을 div 생성
       const container = document.createElement("div");
 
-      // PdfPage 컴포넌트를 실제 DOM으로 변환
       ReactDOM.render(<PdfPage answers={chunk} />, container);
       document.body.appendChild(container);
 
-      // 페이지를 캔버스로 변환 (해상도 두배로 높여서)
       const canvas = await html2canvas(container, { scale: 2 });
-
       const imgData = canvas.toDataURL("image/png");
+
       const imgWidth = pdfWidth - 2 * margin;
       const imgHeight = (canvas.height * imgWidth) / canvas.width;
-
-      const imgx = margin;
-      const imgy = margin;
 
       if (i > 0) {
         pdf.addPage();
       }
 
-      // 캔버스에서 생성된 이미지를 PDF 파일에 추가
-      pdf.addImage(imgData, "PNG", imgx, imgy, imgWidth, imgHeight);
+      pdf.addImage(imgData, "PNG", margin, margin, imgWidth, imgHeight);
 
-      // 페이지번호 추가
       pdf.setFontSize(10);
       pdf.text(
         `${i + 1} / ${chunks.length}`,
@@ -205,14 +188,11 @@ export default function ScorePreview() {
         { align: "center" }
       );
 
-      // 컨테이너 정리
       ReactDOM.unmountComponentAtNode(container);
       document.body.removeChild(container);
     }
 
     pdf.save("우태형_정보처리기사 2024 기출문제 1-2_성적표.pdf");
-
-    // PDF 생성 완료 후 로딩 모달을 닫기
     setLoadingVisible(false);
   };
 
@@ -235,10 +215,9 @@ export default function ScorePreview() {
         </section>
       </div>
 
-      {/* LoadingModal 모달 */}
       <LoadingModal
-        open={loadingVisible} 
-        title="PDF 생성중입니다. 잠시만 기다려주세요." 
+        open={loadingVisible}
+        title="PDF 생성중입니다. 잠시만 기다려주세요."
       />
     </div>
   );
