b88a65d636291b3603d01edfa5b831058d3adf23
ynnotun <ynnotun@gmail.com>
2024-08-28 15:03:17 +0900

    머지 후 오류 해결

--- src/main/java/org/example/final1/repository/BookRepository.java
+++ src/main/java/org/example/final1/repository/BookRepository.java
@@ -9,15 +9,3 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<BookDto, Integer> {
     List<BookDto> findByCategoryCategoryId(Integer categoryId);
 }
-// package org.example.final1.repository;
-
-// import org.example.final1.model.BookDto;
-// import org.example.final1.model.CategoryDto;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import java.util.List;
-// import org.springframework.stereotype.Repository;
-
-
-// public interface BookRepository extends JpaRepository<BookDto, Integer>{
-//     List<BookDto> findByCategory(CategoryDto category);
-// }
