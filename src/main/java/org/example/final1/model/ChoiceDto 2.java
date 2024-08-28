2daa95e3de85abf79348b22167a243017cf8e223
alswl11 <parkminji08@naver.com>
2024-08-28 14:35:51 +0900

    잘못된 파일 delete

---
+++ src/main/java/org/example/final1/model/ChoiceDto 2.java
@@ -0,0 +1,27 @@
+package org.example.final1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_choice")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChoiceDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "choice_id")
+    private int choiceId;
+
+    @Column(name = "choice_text", nullable = false, length = 255)
+    private String choiceText;
+
+    @Column(name = "choice_image", length = 255)
+    private String choiceImage;
+
+}
---
+++ src/main/reactjs/src/routes/study/Study 2.jsx
@@ -0,0 +1,14 @@
+import MainHeader from "../../components/MainHeader";
+import {Outlet} from "react-router-dom";
+import CategoryHeader from "../../components/CategoryHeader";
+
+export default function Study() {
+    return (
+        <div className="min-h-screen bg-white">
+            <MainHeader/>
+            <div className="p-16">
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
