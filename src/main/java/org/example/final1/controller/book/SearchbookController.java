551ade364fdb721d750bd076b4859b0e4bcb9a4b
ynnotun <ynnotun@gmail.com>
2024-09-01 22:09:44 +0900

    책 검색 기능

--- src/main/java/org/example/final1/controller/book/SearchbookController.java
+++ src/main/java/org/example/final1/controller/book/SearchbookController.java
@@ -1,4 +1,22 @@
 package org.example.final1.controller.book;
 
+import org.example.final1.model.BookDto;
+import org.example.final1.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class SearchbookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books/search")
+    public List<BookDto> searchBooks(@RequestParam("keyword") String keyword) {
+        return bookService.searchBooks(keyword);
+    }
 }
