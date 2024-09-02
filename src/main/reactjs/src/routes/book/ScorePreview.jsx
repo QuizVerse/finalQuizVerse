fbc44ea272e732eb2aa440b722199a5c912ae9cb
alswl11 <parkminji08@naver.com>
2024-09-02 20:12:03 +0900

    navigator 수정

--- src/main/reactjs/src/routes/book/ScorePreview.jsx
+++ src/main/reactjs/src/routes/book/ScorePreview.jsx
@@ -70,7 +70,6 @@ export default function ScorePreview() {
       document.body.appendChild(container);
 
       const startNumber = i*80+1;
-      // const endNumber = Math.min(startNumber + 79, answers.length);
 
       // pdfpage 컴포넌트 내용을 div(container)에 넣기, 문제 번호가 올바르게 표시되도록 시작 번호 전달
       ReactDOM.render(
@@ -131,7 +130,7 @@ export default function ScorePreview() {
     <div className="max-w-4xl mx-auto p-4">
       <div className="">
         <div className="flex justify-end space-x-2 mb-4">
-          <Button variant="outlined" onClick={() => navigate("/book/score")}>
+          <Button variant="outlined" onClick={() => navigate(`/book/score/${bookId}`)}>
             성적표로 돌아가기
           </Button>
           <Button variant="outlined" onClick={downloadpdf}>
