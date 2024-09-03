4ab989606e895e29e41a52d4122a2adb59320366
alswl11 <parkminji08@naver.com>
2024-09-03 15:12:33 +0900

    0903

--- src/main/reactjs/src/routes/study/StudyList.jsx
+++ src/main/reactjs/src/routes/study/StudyList.jsx
@@ -3,6 +3,8 @@ import StudyRoomCard from "../../components/StudyRoomCard";
 import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
 import React, {useEffect, useState} from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 const ITEMS_PER_PAGE = 10;
 const SPACING = 2;
@@ -11,12 +13,12 @@ const SPACING = 2;
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export default function StudyList() {
-
     // pagenation에 필요한 변수
     const [page, setPage] = useState(1);
     const itemOffset = (page - 1) * ITEMS_PER_PAGE;
     const currentItems = items.slice(itemOffset, itemOffset + ITEMS_PER_PAGE);
     const pageCount = Math.ceil(items.length / ITEMS_PER_PAGE);
+    const navi = useNavigate();
 
     /**
      * @description : pagenation에 필요한 함수
@@ -26,15 +28,17 @@ export default function StudyList() {
         window.scrollTo(0, 0);
     };
 
-    // const getRoomList = () => {
-    //     axios.get(`/study/list`).then((res) => {
-    //       setRoomList(res.data);
-    //     });
-    //   };
-    
-    //   useEffect(() => {
-    //     getRoomList();
-    //   }, []);
+    const [roomList, setRoomList] = useState([]);
+
+    const getRoomList = () => {
+        axios.get(`/studys/lists`).then((res) => {
+            setRoomList(res.data);
+        });
+    };
+
+    useEffect(() => {
+        getRoomList();
+    }, []);
 
     return (
         <main className="flex flex-col items-center w-full p-4 md:p-10">
@@ -43,15 +47,16 @@ export default function StudyList() {
             </div>
             <div className="grid grid-cols-2 w-full max-w-5xl gap-4">
                 {/* pagenation 적용할 리스트 */}
-                {currentItems &&
-                    currentItems.map((item, index) => (
+                {
+                    roomList.map((item, index) => (
                         <StudyRoomCard
                             key={index}
-                            title={"야호"}
-                            description={"야호"}
+                            title={item.studyTitle}
+                            description={item.studyDescription}
                             nowMember={0}
-                            totalMember={0}
+                            totalMember={item.studyMemberlimit}
                             status={1}
+                            onClick
                         />
                     ))}
             </div>
