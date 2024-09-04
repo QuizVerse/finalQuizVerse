d006a9b3f9922a76f78171aae01d967a31db8663
sanghyuck <djfh611@naver.com>
2024-09-04 19:39:13 +0900

    help

--- src/main/java/org/example/final1/service/PublishedBookService.java
+++ src/main/java/org/example/final1/service/PublishedBookService.java
@@ -2,6 +2,7 @@ package org.example.final1.service;
 
 import org.example.final1.model.BookDto;
 import org.example.final1.repository.PublishedBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public class PublishedBookService {
 
     private final PublishedBookRepository publishedBookRepository;
 
+    @Autowired
     public PublishedBookService(PublishedBookRepository publishedBookRepository) {
         this.publishedBookRepository = publishedBookRepository;
     }
