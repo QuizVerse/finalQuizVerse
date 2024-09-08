e5bb20c275033d1584c1a148e4f26f59a67ca949
hayoonii <rkdgkdbs01@naver.com>
2024-09-09 02:23:36 +0900

    wrong url error

--- src/main/java/org/example/final1/repository/SolvedbookRepository.java
+++ src/main/java/org/example/final1/repository/SolvedbookRepository.java
@@ -1,6 +1,8 @@
 package org.example.final1.repository;
 
+import org.example.final1.model.BookDto;
 import org.example.final1.model.SolvedbookDto;
+import org.example.final1.model.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +12,7 @@ public interface SolvedbookRepository extends JpaRepository<SolvedbookDto, Integ
     // 사용자 ID가 응시한 문제집 총 개수 가져오기
     @Query("select count(c) from SolvedbookDto c where c.user.userId=:userId")
     int countSolvedBookByUserId(@Param("userId")int userId);
+
+    // 사용자와 bookId로 이미 풀었던 기록을 찾는 쿼리
+    SolvedbookDto findByUserAndBook(UserDto userDto, BookDto bookDto);
 }
