7b19ff0fb91b1bf29d6bd78ba28fd7830423502b
hayoonii <rkdgkdbs01@naver.com>
2024-09-08 22:38:19 +0900

    wrong_repeat 구현

--- src/main/java/org/example/final1/service/AnswerService.java
+++ src/main/java/org/example/final1/service/AnswerService.java
@@ -9,7 +9,6 @@ import org.example.final1.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
