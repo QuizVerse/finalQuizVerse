a3e662a3d3b76e556d3657663fd28327ac6db3dd
ynnotun <ynnotun@gmail.com>
2024-09-11 17:23:00 +0900

    배너 클릭시 넘어가도록 만들음!

--- src/main/reactjs/src/routes/book/book.jsx
+++ src/main/reactjs/src/routes/book/book.jsx
@@ -1,11 +1,11 @@
 import MainHeader from "../../components/MainHeader";
 import { Outlet, useLocation } from "react-router-dom";
 import CategoryHeader from "../../components/CategoryHeader";
-import Chatbot from "../../components/Chatbot";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Pagination} from "swiper/modules";
 import React from "react";
-import MainFooter from "../../components/MainFooter";  // 챗봇 컴포넌트 임포트
+import MainFooter from "../../components/MainFooter";
+import {Link} from "react-router-dom";  // react-router-dom에서 Link 컴포넌트 임포트
 
 export default function Book() {
     const location = useLocation();
@@ -28,22 +28,19 @@ export default function Book() {
                     style={{ height: '500px' }}
                 >
                     <SwiperSlide>
-                        <img
-                            src="/banner2.png"
-                            style={{cursor: 'pointer'}}
-                        />
+                        <Link to={"/account/login"} className={"w-full"}>
+                            <img src="/banner2.png"/>
+                        </Link>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img
-                            src="/banner1.png"
-                            style={{cursor: 'pointer'}}
-                        />
+                        <Link to={"/account/login"} className={"w-full"}>
+                            <img src="/banner1.png"/>
+                        </Link>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img
-                            src="/banner3.png"
-                            style={{cursor: 'pointer'}}
-                        />
+                        <Link to={"/study/list"} className={"w-full"}>
+                            <img src="/banner3.png"/>
+                        </Link>
                     </SwiperSlide>
                 </Swiper>
             )}
@@ -51,11 +48,6 @@ export default function Book() {
             <div className="p-16">
                 <Outlet />
             </div>
-
-            {/* 챗봇을 화면의 오른쪽 하단에 고정 */}
-            {/*<div className="fixed bottom-4 right-4">*/}
-            {/*    <Chatbot />*/}
-            {/*</div>*/}
             <MainFooter/>
         </div>
     );
