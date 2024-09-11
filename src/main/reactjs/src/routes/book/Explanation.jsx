e2c349986b873a8a05abc4acfb1d899bc76bfc95
taehyoung809 <soona809@naver.com>
2024-09-11 16:30:50 +0900

    wrongRepeat 넣어서 navigate

--- src/main/reactjs/src/routes/book/Explanation.jsx
+++ src/main/reactjs/src/routes/book/Explanation.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import jsPDF from 'jspdf';
-import { useParams, Link } from 'react-router-dom';
+import {useParams, Link, useLocation} from 'react-router-dom';
 import { Button, Typography } from "@mui/material";
 import PanoramaFishEyeOutlinedIcon from '@mui/icons-material/PanoramaFishEyeOutlined';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
@@ -14,6 +14,10 @@ const Explanation = () => {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState({});
   const [selectedAnswers, setSelectedAnswers] = useState({});  // 사용자의 선택된 답안
+  const { search } = useLocation();
+  const queryParmas=new URLSearchParams(search);
+  const wrongRepeat=queryParmas.get("wrongRepeat");
+
 
   // 응시자 정보를 가져오는 함수
   const fetchUserData = async () => {
@@ -168,7 +172,7 @@ const Explanation = () => {
       <div className="max-w-4xl mx-auto p-4">
         <div className="flex justify-end">
           <Button variant="outlined">
-            <Link to={`/book/score/${bookId}/${solvedbookId}`}>성적보기</Link>  {/* Link 컴포넌트 사용 */}
+            <Link to={`/book/score/${bookId}/${solvedbookId}?wrongRepeat=${wrongRepeat}`}>성적보기</Link>  {/* Link 컴포넌트 사용 */}
           </Button>
         </div>
         <div>
