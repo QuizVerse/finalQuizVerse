d072ebe6d726399af2887e0eabe73596b65c6663
alswl11 <parkminji08@naver.com>
2024-09-12 10:20:18 +0900

    문제집 생성 표지 페이지 디자인 수정

--- src/main/reactjs/src/routes/book/NewBook.jsx
+++ src/main/reactjs/src/routes/book/NewBook.jsx
@@ -7,7 +7,7 @@ import {
     InputLabel,
     FormControl,
     TextField,
-    IconButton
+    IconButton, Switch, Tooltip
 } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
 import CreateIcon from '@mui/icons-material/Create';
