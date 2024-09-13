e5dfdae17e8730ec8843a0c3fd7c517957c78154
ynnotun <ynnotun@gmail.com>
2024-09-13 11:54:01 +0900

    pdf 출력 버튼 돌아가는 버튼으로 수정

--- src/main/reactjs/src/routes/book/QuestionPreviewPDF.jsx
+++ src/main/reactjs/src/routes/book/QuestionPreviewPDF.jsx
@@ -114,7 +114,9 @@ const QuestionPreviewPDF = () => {
     return (
         <div className="max-w-4xl mx-auto p-4">
             <div className={"flex justify-end"} >
-            <Button variant={"outlined"} onClick={generatePDF}>PDF로 출력</Button>
+            <Button variant={"outlined"}>
+                <Link to={"/book/questionpreview"+bookId}>문제집 미리보기</Link>
+            </Button>
             </div>
             <div>
                 <div className="overflow-x-auto pt-5 py-8">
